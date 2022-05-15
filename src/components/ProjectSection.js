bbfa80cadf833128f9f1e02c6f22f955dea53382
matheusm <matheus.martins1991@gmail.com>
2022-05-14 23:56:40 -0300

    Fixed issues regarding responsivity in medium screen devices

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -5,7 +5,9 @@ export default function ProjectSection() {
 
   return (
     <section className="section section--flex-row">
-      <h1 className="section__title">Projects</h1>
+      <div className="section__container">
+        <h1 className="section__title">Projects</h1>
+      </div>
       <div className="section__container section__container--overflow">
         {projects.map(project => 
           <ProjectCard 
